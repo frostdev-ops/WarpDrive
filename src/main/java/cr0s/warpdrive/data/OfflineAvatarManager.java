4d8ebca642abdae02bf15eda1db5cb063ba022f8
frostdev-ops <frostdev-ops@users.noreply.github.com>
2026-06-29 16:43:29 -0400

    Fix offline avatar Morph registry spoofing

--- src/main/java/cr0s/warpdrive/data/OfflineAvatarManager.java
+++ src/main/java/cr0s/warpdrive/data/OfflineAvatarManager.java
@@ -25,9 +25,54 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
 
 public class OfflineAvatarManager {
-	
+
 	private static final HashMap<UUID, GlobalPosition> registry = new HashMap<>(512);
-	
+	private static final HashMap<UUID, UUID> registryEntityIds = new HashMap<>(512);
+
+	private static boolean isValidPosition(@Nonnull final GlobalPosition globalPosition) {
+		return globalPosition.y >= 0 && globalPosition.y < 256;
+	}
+
+	private static boolean isValidPosition(@Nonnull final EntityOfflineAvatar entityOfflineAvatar) {
+		return entityOfflineAvatar.posY >= 0.0D && entityOfflineAvatar.posY < entityOfflineAvatar.world.getHeight();
+	}
+
+	private static boolean isAtPosition(@Nonnull final EntityOfflineAvatar entityOfflineAvatar,
+	                                    @Nonnull final GlobalPosition globalPosition) {
+		return globalPosition.dimensionId == entityOfflineAvatar.world.provider.getDimension()
+		    && globalPosition.x == (int) Math.floor(entityOfflineAvatar.posX)
+		    && globalPosition.y == (int) Math.floor(entityOfflineAvatar.posY)
+		    && globalPosition.z == (int) Math.floor(entityOfflineAvatar.posZ);
+	}
+
+	private static boolean bindLegacyAvatar(@Nonnull final UUID uuidPlayer,
+	                                        @Nonnull final EntityOfflineAvatar entityOfflineAvatar,
+	                                        @Nonnull final GlobalPosition globalPosition) {
+		if (!isAtPosition(entityOfflineAvatar, globalPosition)) {
+			return false;
+		}
+
+		final List<EntityOfflineAvatar> entityOfflineAvatars = entityOfflineAvatar.world.getEntities(
+				EntityOfflineAvatar.class,
+				entity -> entity != null
+				       && entity.isEntityAlive()
+				       && uuidPlayer.equals(entity.getPlayerUUID())
+				       && isAtPosition(entity, globalPosition) );
+		if ( entityOfflineAvatars.size() != 1
+		  || entityOfflineAvatars.get(0) != entityOfflineAvatar ) {
+			return false;
+		}
+
+		entityOfflineAvatar.setManagedOfflineAvatar();
+		registryEntityIds.put(uuidPlayer, entityOfflineAvatar.getUniqueID());
+		if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
+			WarpDrive.logger.info(String.format("Bound legacy offline avatar for %s (%s) to entity %s at %s",
+			                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
+			                                    entityOfflineAvatar.getUniqueID(), Commons.format(globalPosition) ));
+		}
+		return true;
+	}
+
 	public static void update(@Nonnull final EntityOfflineAvatar entityOfflineAvatar) {
 		// validate context
 		if (!Commons.isSafeThread()) {
@@ -40,31 +85,81 @@ public class OfflineAvatarManager {
 			                                     entityOfflineAvatar ));
 			return;
 		}
-		
+		if (!isValidPosition(entityOfflineAvatar)) {
+			if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
+				WarpDrive.logger.warn(String.format("Ignoring update for out-of-world EntityOfflineAvatar for %s (%s) at %s",
+				                                    entityOfflineAvatar.getPlayerName(), entityOfflineAvatar.getPlayerUUID(),
+				                                    Commons.format(entityOfflineAvatar) ));
+			}
+			return;
+		}
+
 		// add new entry
 		// or update existing entry
-		final GlobalPosition globalPositionActual = registry.get(entityOfflineAvatar.getPlayerUUID());
+		final UUID uuidPlayer = entityOfflineAvatar.getPlayerUUID();
+		final UUID uuidEntity = entityOfflineAvatar.getUniqueID();
+		final GlobalPosition globalPositionActual = registry.get(uuidPlayer);
+		final UUID uuidEntityActual = registryEntityIds.get(uuidPlayer);
+		if ( uuidEntityActual != null
+		  && !uuidEntityActual.equals(uuidEntity) ) {
+			if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
+				WarpDrive.logger.warn(String.format("Ignoring update from duplicate EntityOfflineAvatar for %s (%s): expected entity %s, got %s at %s",
+				                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
+				                                    uuidEntityActual, uuidEntity,
+				                                    Commons.format(entityOfflineAvatar) ));
+			}
+			return;
+		}
+		if ( uuidEntityActual == null
+		  && globalPositionActual != null
+		  && globalPositionActual.distance2To(entityOfflineAvatar) > 1.0D ) {
+			if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
+				WarpDrive.logger.warn(String.format("Ignoring update from unbound EntityOfflineAvatar for %s (%s): registered at %s, got entity %s at %s",
+				                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
+				                                    Commons.format(globalPositionActual), uuidEntity,
+				                                    Commons.format(entityOfflineAvatar) ));
+			}
+			return;
+		}
+		if ( uuidEntityActual == null
+		  && !entityOfflineAvatar.isManagedOfflineAvatar() ) {
+			if ( globalPositionActual == null
+			  || !bindLegacyAvatar(uuidPlayer, entityOfflineAvatar, globalPositionActual) ) {
+				if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
+					WarpDrive.logger.warn(String.format("Ignoring update from unmanaged EntityOfflineAvatar for %s (%s): entity %s at %s",
+					                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
+					                                    uuidEntity,
+					                                    Commons.format(entityOfflineAvatar) ));
+				}
+				return;
+			}
+		}
+		if ( uuidEntityActual == null
+		  && globalPositionActual != null ) {
+			registryEntityIds.put(uuidPlayer, uuidEntity);
+		}
 		if ( globalPositionActual == null
 		  || globalPositionActual.dimensionId != entityOfflineAvatar.world.provider.getDimension()
 		  || globalPositionActual.x != (int) Math.floor(entityOfflineAvatar.posX)
 		  || globalPositionActual.y != (int) Math.floor(entityOfflineAvatar.posY)
 		  || globalPositionActual.z != (int) Math.floor(entityOfflineAvatar.posZ) ) {
 			final GlobalPosition globalPositionUpdated = new GlobalPosition(entityOfflineAvatar);
-			registry.put(entityOfflineAvatar.getPlayerUUID(), globalPositionUpdated);
+			registry.put(uuidPlayer, globalPositionUpdated);
+			registryEntityIds.put(uuidPlayer, uuidEntity);
 			if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
 				if (globalPositionActual == null) {
 					WarpDrive.logger.info(String.format("Added offline avatar for %s (%s) %s",
-					                                    entityOfflineAvatar.getPlayerName(), entityOfflineAvatar.getPlayerUUID(),
+					                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
 					                                    Commons.format(globalPositionUpdated) ));
 				} else {
 					WarpDrive.logger.info(String.format("Updated offline avatar for %s (%s) from %s to %s",
-					                                    entityOfflineAvatar.getPlayerName(), entityOfflineAvatar.getPlayerUUID(),
+					                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
 					                                    Commons.format(globalPositionActual), Commons.format(globalPositionUpdated) ));
 				}
 			}
 		}
 	}
-	
+
 	public static void remove(@Nonnull final EntityOfflineAvatar entityOfflineAvatar) {
 		// validate context
 		if (!Commons.isSafeThread()) {
@@ -77,33 +172,62 @@ public class OfflineAvatarManager {
 			                                     entityOfflineAvatar ));
 			return;
 		}
-		
+
 		// remove existing entry, if coordinates are matching
-		final GlobalPosition globalPosition = registry.get(entityOfflineAvatar.getPlayerUUID());
+		final UUID uuidPlayer = entityOfflineAvatar.getPlayerUUID();
+		final UUID uuidEntity = entityOfflineAvatar.getUniqueID();
+		final GlobalPosition globalPosition = registry.get(uuidPlayer);
+		final UUID uuidEntityActual = registryEntityIds.get(uuidPlayer);
 		if ( globalPosition != null
 		  && globalPosition.dimensionId == entityOfflineAvatar.world.provider.getDimension()
 		  && globalPosition.x == (int) Math.floor(entityOfflineAvatar.posX)
 		  && globalPosition.y == (int) Math.floor(entityOfflineAvatar.posY)
-		  && globalPosition.z == (int) Math.floor(entityOfflineAvatar.posZ) ) {
-			registry.remove(entityOfflineAvatar.getPlayerUUID());
+		  && globalPosition.z == (int) Math.floor(entityOfflineAvatar.posZ)
+		  && uuidEntityActual != null
+		  && uuidEntityActual.equals(uuidEntity) ) {
+			registry.remove(uuidPlayer);
+			registryEntityIds.remove(uuidPlayer);
 			if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
 				WarpDrive.logger.info(String.format("Removed offline avatar registration for %s (%s) %s",
-				                                    entityOfflineAvatar.getPlayerName(), entityOfflineAvatar.getPlayerUUID(),
+				                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
 				                                    entityOfflineAvatar ));
 			}
 		}
 	}
-	
+
+	public static void release(@Nonnull final EntityOfflineAvatar entityOfflineAvatar) {
+		if (!Commons.isSafeThread()) {
+			WarpDrive.logger.error(String.format("Non-threadsafe call to OfflineAvatarManager:release outside main thread, for %s",
+			                                     entityOfflineAvatar ));
+			return;
+		}
+		final UUID uuidPlayer = entityOfflineAvatar.getPlayerUUID();
+		if (uuidPlayer == null) {
+			return;
+		}
+		final UUID uuidEntityActual = registryEntityIds.get(uuidPlayer);
+		if ( uuidEntityActual != null
+		  && uuidEntityActual.equals(entityOfflineAvatar.getUniqueID()) ) {
+			registryEntityIds.remove(uuidPlayer);
+			if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
+				WarpDrive.logger.info(String.format("Released offline avatar entity binding for %s (%s) %s",
+				                                    entityOfflineAvatar.getPlayerName(), uuidPlayer,
+				                                    entityOfflineAvatar ));
+			}
+		}
+	}
+
 	private static void remove(@Nonnull final EntityPlayer entityPlayer) {
 		// update registry
 		final GlobalPosition globalPosition = registry.remove(entityPlayer.getUniqueID());
+		registryEntityIds.remove(entityPlayer.getUniqueID());
 		if ( globalPosition != null
 		  && WarpDriveConfig.LOGGING_OFFLINE_AVATAR ) {
 			WarpDrive.logger.info(String.format("Removed offline avatar registration for %s (%s) %s",
 			                                    entityPlayer.getName(), entityPlayer.getUniqueID(),
 			                                    Commons.format(globalPosition) ));
 		}
-		
+
 		// remove supporting entity
 		final List<EntityOfflineAvatar> entityOfflineAvatars = entityPlayer.world.getEntities(
 				EntityOfflineAvatar.class,
@@ -114,7 +238,7 @@ public class OfflineAvatarManager {
 			entityOfflineAvatar.setDead();
 		}
 	}
-	
+
 	@Nullable
 	public static GlobalPosition get(@Nonnull final UUID uuidPlayer) {
 		// validate context
@@ -123,58 +247,71 @@ public class OfflineAvatarManager {
 			                                     uuidPlayer ));
 			return null;
 		}
-		
+
 		// get existing entry
 		return registry.get(uuidPlayer);
 	}
-	
+
 	public static void readFromNBT(@Nullable final NBTTagCompound tagCompound) {
 		if ( tagCompound == null
 		  || !tagCompound.hasKey("offlineAvatars") ) {
 			registry.clear();
+			registryEntityIds.clear();
 			return;
 		}
-		
+
 		// read all entries in a pre-build local collections using known stats to avoid re-allocations
 		final NBTTagList tagList = tagCompound.getTagList("offlineAvatars", Constants.NBT.TAG_COMPOUND);
 		final HashMap<UUID, GlobalPosition> registryLocal = new HashMap<>(tagList.tagCount());
+		final HashMap<UUID, UUID> registryEntityIdsLocal = new HashMap<>(tagList.tagCount());
 		for (int index = 0; index < tagList.tagCount(); index++) {
 			final NBTTagCompound tagCompoundItem = tagList.getCompoundTagAt(index);
 			final UUID uuid = tagCompoundItem.getUniqueId("");
 			final GlobalPosition globalPosition = new GlobalPosition(tagCompoundItem);
+			if (!isValidPosition(globalPosition)) {
+				WarpDrive.logger.warn(String.format("Discarding invalid offline avatar registration for %s at %s",
+				                                    uuid, Commons.format(globalPosition) ));
+				continue;
+			}
 			registryLocal.put(uuid, globalPosition);
+			if (tagCompoundItem.hasUniqueId("entity")) {
+				registryEntityIdsLocal.put(uuid, tagCompoundItem.getUniqueId("entity"));
+			}
 		}
-		
+
 		// transfer to main one
 		registry.clear();
 		registry.putAll(registryLocal);
-		for (final Entry<UUID, GlobalPosition> entry : registryLocal.entrySet()) {
-			registry.put(entry.getKey(), entry.getValue());
-		}
+		registryEntityIds.clear();
+		registryEntityIds.putAll(registryEntityIdsLocal);
 	}
-	
+
 	public static void writeToNBT(@Nonnull final NBTTagCompound tagCompound) {
 		final NBTTagList tagList = new NBTTagList();
 		for (final Entry<UUID, GlobalPosition> entry : registry.entrySet()) {
 			final NBTTagCompound tagCompoundItem = new NBTTagCompound();
 			tagCompoundItem.setUniqueId("", entry.getKey());
 			entry.getValue().writeToNBT(tagCompoundItem);
+			final UUID uuidEntity = registryEntityIds.get(entry.getKey());
+			if (uuidEntity != null) {
+				tagCompoundItem.setUniqueId("entity", uuidEntity);
+			}
 			tagList.appendTag(tagCompoundItem);
 		}
 		tagCompound.setTag("offlineAvatars", tagList);
 	}
-	
+
 	public static void onPlayerLoggedOut(@Nonnull final EntityPlayer entityPlayer) {
 		// skip dead players
 		if (entityPlayer.isDead) {
 			if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
 				WarpDrive.logger.info(String.format("Skipping offline avatar for dead player %s",
 				                                    entityPlayer ));
-				
+
 			}
 			return;
 		}
-		
+
 		// skip players away from a ship
 		final World world = entityPlayer.world;
 		final BlockPos blockPos = entityPlayer.getPosition();
@@ -189,7 +326,7 @@ public class OfflineAvatarManager {
 				return;
 			}
 		}
-		
+
 		// spawn an offline avatar entity at location
 		// note: we don't check if one is already there since all avatars will be removed after successful reconnection anyway
 		WarpDrive.logger.debug(String.format("Spawning offline avatar for %s",
@@ -199,6 +336,7 @@ public class OfflineAvatarManager {
 		                                           entityPlayer.rotationYaw, entityPlayer.rotationPitch );
 		entityOfflineAvatar.setCustomNameTag(entityPlayer.getDisplayNameString());
 		entityOfflineAvatar.setPlayer(entityPlayer.getUniqueID(), entityPlayer.getName());
+		entityOfflineAvatar.setManagedOfflineAvatar();
 		entityOfflineAvatar.setInvisible(entityPlayer.isSpectator());
 		entityOfflineAvatar.setEntityInvulnerable(entityPlayer.isCreative() || entityPlayer.isSpectator());
 		// copy equipment with a marker to remember those aren't 'legit' items
@@ -226,16 +364,24 @@ public class OfflineAvatarManager {
 			}
 		}
 	}
-	
+
 	public static void onPlayerLoggedIn(@Nonnull final EntityPlayer entityPlayer) {
 		assert !entityPlayer.isAddedToWorld();
-		
+
 		// skip if we have no record
 		final GlobalPosition globalPosition = registry.get(entityPlayer.getUniqueID());
 		if (globalPosition == null) {
 			return;
 		}
-		
+		if (!isValidPosition(globalPosition)) {
+			registry.remove(entityPlayer.getUniqueID());
+			registryEntityIds.remove(entityPlayer.getUniqueID());
+			WarpDrive.logger.warn(String.format("Discarding invalid offline avatar registration for %s (%s) at %s",
+			                                    entityPlayer.getName(), entityPlayer.getUniqueID(),
+			                                    Commons.format(globalPosition) ));
+			return;
+		}
+
 		// skip if player is already close by
 		if (globalPosition.dimensionId == entityPlayer.dimension) {
 			final double distance = entityPlayer.getDistance(globalPosition.x + 0.5D, globalPosition.y, globalPosition.z + 0.5D);
@@ -246,7 +392,7 @@ public class OfflineAvatarManager {
 				return;
 			}
 		}
-		
+
 		// teleport player
 		// note: this is done before server loads the player's world
 		if (WarpDriveConfig.LOGGING_OFFLINE_AVATAR) {
@@ -260,7 +406,7 @@ public class OfflineAvatarManager {
 			remove(entityPlayer);
 		}
 	}
-	
+
 	private static boolean isInRange(@Nonnull final EntityPlayer entityPlayer, @Nonnull final GlobalPosition globalPosition) {
 		final float dX = (float) (entityPlayer.posX - globalPosition.x);
 		final float dY = (float) (entityPlayer.posY - globalPosition.y);
@@ -269,16 +415,24 @@ public class OfflineAvatarManager {
 		return distance >= WarpDriveConfig.OFFLINE_AVATAR_MIN_RANGE_FOR_REMOVAL
 		    && distance <= WarpDriveConfig.OFFLINE_AVATAR_MAX_RANGE_FOR_REMOVAL;
 	}
-	
+
 	public static void onTick(@Nonnull final EntityPlayer entityPlayer) {
 		if ( WarpDriveConfig.OFFLINE_AVATAR_DELAY_FOR_REMOVAL_TICKS == 0
 		  || entityPlayer.ticksExisted == 0
 		  || (entityPlayer.ticksExisted % WarpDriveConfig.OFFLINE_AVATAR_DELAY_FOR_REMOVAL_TICKS) != 0 ) {
 			return;
 		}
-		
+
 		final GlobalPosition globalPosition = registry.get(entityPlayer.getUniqueID());
 		if (globalPosition == null) {
+			return;
+		}
+		if (!isValidPosition(globalPosition)) {
+			registry.remove(entityPlayer.getUniqueID());
+			registryEntityIds.remove(entityPlayer.getUniqueID());
+			WarpDrive.logger.warn(String.format("Discarding invalid offline avatar registration for %s (%s) at %s",
+			                                    entityPlayer.getName(), entityPlayer.getUniqueID(),
+			                                    Commons.format(globalPosition) ));
 			return;
 		}
 		if ( globalPosition.dimensionId == entityPlayer.world.provider.getDimension()
--- src/main/java/cr0s/warpdrive/entity/EntityOfflineAvatar.java
+++ src/main/java/cr0s/warpdrive/entity/EntityOfflineAvatar.java
@@ -19,68 +19,78 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 import com.google.common.base.Optional;
 
 public class EntityOfflineAvatar extends EntityLiving {
-	
+
 	// persistent properties
 	private static final DataParameter<Optional<UUID>> DATA_PLAYER_UUID = EntityDataManager.createKey(EntityOfflineAvatar.class, DataSerializers.OPTIONAL_UNIQUE_ID);
 	private static final DataParameter<String>         DATA_PLAYER_NAME = EntityDataManager.createKey(EntityOfflineAvatar.class, DataSerializers.STRING);
-	
+
 	// computed properties
 	private GlobalPosition cache_globalPosition;
 	private boolean isDirtyGlobalPosition = true;
 	private int tickUpdateGlobalPosition = 0;
-	
+	private boolean isManagedOfflineAvatar = false;
+
 	public EntityOfflineAvatar(@Nonnull final World world) {
 		super(world);
-		
+
 		setCanPickUpLoot(false);
 		setNoAI(true);
 		setCustomNameTag("Offline avatar");
 		setAlwaysRenderNameTag(WarpDriveConfig.OFFLINE_AVATAR_ALWAYS_RENDER_NAME_TAG);
 		setSize(width * WarpDriveConfig.OFFLINE_AVATAR_MODEL_SCALE, height  * WarpDriveConfig.OFFLINE_AVATAR_MODEL_SCALE);
 	}
-	
+
 	@Override
 	protected void entityInit() {
 		super.entityInit();
-		
+
 		dataManager.register(DATA_PLAYER_UUID, Optional.absent());
 		dataManager.register(DATA_PLAYER_NAME, "");
 	}
-	
+
 	public void setPlayer(@Nonnull final UUID uuidPlayer, @Nonnull final String namePlayer) {
 		dataManager.set(DATA_PLAYER_UUID, Optional.of(uuidPlayer));
 		dataManager.set(DATA_PLAYER_NAME, namePlayer);
 	}
-	
+
+	public void setManagedOfflineAvatar() {
+		isManagedOfflineAvatar = true;
+	}
+
+	public boolean isManagedOfflineAvatar() {
+		return isManagedOfflineAvatar;
+	}
+
 	@Nullable
 	public UUID getPlayerUUID() {
 		return dataManager.get(DATA_PLAYER_UUID).orNull();
 	}
-	
+
 	public String getPlayerName() {
 		return dataManager.get(DATA_PLAYER_NAME);
 	}
-	
+
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
-		
+
 		if (world.isRemote) {
 			return;
 		}
-		
+
 		// detect position change
 		if ( cache_globalPosition == null
 		  || cache_globalPosition.distance2To(this) > 1.0D ) {
 			isDirtyGlobalPosition = true;
 			cache_globalPosition = new GlobalPosition(this);
 		}
-		
+
 		// update registry
 		if (isDirtyGlobalPosition) {
 			tickUpdateGlobalPosition = 0;
@@ -89,7 +99,7 @@ public class EntityOfflineAvatar extends EntityLiving {
 		if (tickUpdateGlobalPosition <= 0) {
 			tickUpdateGlobalPosition = WarpDriveConfig.G_REGISTRY_UPDATE_INTERVAL_TICKS;
 			isDirtyGlobalPosition = false;
-			
+
 			final UUID uuidPlayer = getPlayerUUID();
 			if (uuidPlayer == null) {
 				// cleanup invalid entities
@@ -98,7 +108,7 @@ public class EntityOfflineAvatar extends EntityLiving {
 					                                     this ));
 					setDead();
 				}
-				
+
 			} else {
 				// update registry
 				// note: since offline avatars can be killed while keeping last known position, the removal is handled by the manager
@@ -106,25 +116,42 @@ public class EntityOfflineAvatar extends EntityLiving {
 			}
 		}
 	}
-	
+
+	@Override
+	public boolean attackEntityFrom(@Nonnull final DamageSource damageSource, final float amount) {
+		if ( amount <= 0.0F
+		  || isEntityInvulnerable(damageSource) ) {
+			return false;
+		}
+		if (!world.isRemote) {
+			setDead();
+		}
+		return true;
+	}
+
 	@Override
 	public void setDead() {
 		super.setDead();
-		
+
+		if (world.isRemote) {
+			return;
+		}
 		if (WarpDriveConfig.OFFLINE_AVATAR_FORGET_ON_DEATH) {
 			OfflineAvatarManager.remove(this);
+		} else {
+			OfflineAvatarManager.release(this);
 		}
 	}
-	
+
 	@Override
 	public float getRenderSizeModifier() {
 		return WarpDriveConfig.OFFLINE_AVATAR_MODEL_SCALE;
 	}
-	
+
 	@Override
 	public void readEntityFromNBT(@Nonnull final NBTTagCompound tagCompound) {
 		super.readEntityFromNBT(tagCompound);
-		
+
 		final UUID uuidPlayer = tagCompound.getUniqueId("player");
 		final String namePlayer = tagCompound.getString("playerName");
 		if ( uuidPlayer == null
@@ -135,7 +162,9 @@ public class EntityOfflineAvatar extends EntityLiving {
 			return;
 		}
 		setPlayer(uuidPlayer, namePlayer);
-		
+		isManagedOfflineAvatar = tagCompound.hasUniqueId("avatarEntity")
+		                      && getUniqueID().equals(tagCompound.getUniqueId("avatarEntity"));
+
 		for (final EntityEquipmentSlot entityEquipmentSlot : EntityEquipmentSlot.values()) {
 			final ItemStack itemStack = getItemStackFromSlot(entityEquipmentSlot);
 			if ( !itemStack.isEmpty()
@@ -144,11 +173,11 @@ public class EntityOfflineAvatar extends EntityLiving {
 			}
 		}
 	}
-	
+
 	@Override
 	public void writeEntityToNBT(@Nonnull final NBTTagCompound tagCompound) {
 		super.writeEntityToNBT(tagCompound);
-		
+
 		final UUID uuidPlayer = getPlayerUUID();
 		if (uuidPlayer == null) {
 			WarpDrive.logger.error(String.format("Removing on writing invalid offline avatar in %s",
@@ -158,23 +187,26 @@ public class EntityOfflineAvatar extends EntityLiving {
 		}
 		tagCompound.setUniqueId("player", uuidPlayer);
 		tagCompound.setString("playerName", getPlayerName());
+		if (isManagedOfflineAvatar) {
+			tagCompound.setUniqueId("avatarEntity", getUniqueID());
+		}
 	}
-	
+
 	@Override
 	public boolean canBeLeashedTo(@Nonnull final EntityPlayer entityPlayer) {
 		return false;
 	}
-	
+
 	@Override
 	protected boolean isMovementBlocked() {
 		return true;
 	}
-	
+
 	@Override
 	public boolean getAlwaysRenderNameTagForRender() {
 		return super.getAlwaysRenderNameTagForRender();
 	}
-	
+
 	@Override
 	public boolean getAlwaysRenderNameTag() {
 		return super.getAlwaysRenderNameTag();
